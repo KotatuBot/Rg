8b88a4c46c272718e897b787b84f21e4fbe394f5
kotatuBot <kotatubot@gmail.com>
2018-05-02 13:34:30 +0900

    made test dir

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         name="rg",
         version="0.1",
         description="Search gadget for pwn problem",
-        url="https://github.com/KotatuBot/Rop_Search",
+        url="https://github.com/KotatuBot/Rg.git",
         packages=find_packages(),
         entry_points={
             'console_scripts':['rg=Rg.rg:main']
